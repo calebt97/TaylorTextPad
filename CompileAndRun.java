d56176b5bc90e9df06bf176c0fe24a35698164ac
Caleb Taylor <calebt97@gmail.com>
2017-11-25 23:50:49 -0500

    Update CompileAndRun.java

--- CompileAndRun.java
+++ CompileAndRun.java
@@ -2,16 +2,19 @@
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-/* Attempts to compile/run the text in the file if saved as a .java file. It uses "javac blank.java" and "java  blank" to
-   do that. Output/Error messages are printed out to the developer console. But the next update will build a console within the text
-   pad. 
+/* Attempts to compile/run the text in the file if saved as a .java file. Also builds the console to display the output
+ and error messages
  */
 public class CompileAndRun {
     String savePath;
     String fileName;
     String withoutExtension;
-    public CompileAndRun(String fileName, String savePath) {
+    OutputConsole console;
 
+
+    public CompileAndRun(String fileName, String savePath) {
+        console = new OutputConsole();
+        console.build();
         this.fileName = fileName;
         this.savePath = savePath;
         String[] tokens = fileName.split("\\.");
@@ -23,43 +26,50 @@ public class CompileAndRun {
             ex.printStackTrace();
         }
     }
-//builds another process to run the compiled class
-    public int run(String classToBeRan) throws IOException, InterruptedException {
+
+    //builds another process to run the compiled class
+    public void run(String classToBeRan) throws IOException, InterruptedException {
+
         ProcessBuilder pb = new ProcessBuilder("java ", classToBeRan);
         pb.redirectErrorStream(true);
-        //Sets the directory.
         pb.directory(new File(savePath));
-        //Starts the process, and appends 
+
         Process p = pb.start();
         InputStreamConsumer consumer = new InputStreamConsumer(p.getInputStream());
         consumer.start();
-        //waits for the process to complete
-        int result = p.waitFor();
-
+        //if you want the return value from the compilation, make it "int result = p.waitFor();"
+        p.waitFor();
         consumer.join();
-        //Prints the results from the process
-        System.out.println(consumer.getOutput());
+        //appends output from running the code
+        console.append(consumer.getOutput().toString());
 
-        return result;
     }
 
-//builds process to compile the file, once saved as a .java
-    public int compile(String file) throws IOException, InterruptedException {
+
+    //builds process to compile the file, once saved as a .java
+    public void compile(String file) throws IOException, InterruptedException {
         ProcessBuilder pb = new ProcessBuilder("javac ", file);
-        //Allows me to get the error messages to be printed out.
+        //If changed to just pb.redirectError(), hides error messages
         pb.redirectErrorStream(true);
-        //Sets the directory for the process, using the savePath String I received in the constructor.
+
         pb.directory(new File(savePath));
         Process p = pb.start();
         InputStreamConsumer consumer = new InputStreamConsumer(p.getInputStream());
         consumer.start();
-        //Waits until everything is done.
-        int result = p.waitFor();
-        consumer.join();
-        //Will print out error messages if necessary.
-        System.out.println(consumer.getOutput());
 
-        return result;
+         p.waitFor();
+        consumer.join();
+        //appends
+        buildCompileConsole(consumer.getOutput().toString());
+
+    }
+
+    private void buildCompileConsole(String input){
+
+        if(!console.doesExist())
+            console.makeVisible(true);
+
+        console.append(input);
     }
 
     public class InputStreamConsumer extends Thread {
